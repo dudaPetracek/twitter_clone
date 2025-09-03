f06985da3756aaa2ffa3fe3e56ba4c8164043383
Duda Petracek <157414482+dudaPetracek@users.noreply.github.com>
2025-09-03 10:44:29 -0300

    Update settings.py

--- twitter_clone/socialmedia/settings.py
+++ twitter_clone/socialmedia/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "twitter",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
